2da37b45552b490e5cbf5b2c4f84e06be6d403fe
Jared <jrddunbr@gmail.com>
2017-09-23 13:49:38 -0400

    found bug with paths and matplotlib

--- net/image/gen_images.py
+++ net/image/gen_images.py
@@ -66,15 +66,15 @@ def graph(datapath, imagepath, title):
     print ("Creating plot: {}".format(imagepath))
     # Save in temp file to prevent waiting for file to be updated on front-facing page
     try:
-        plt.savefig(imagepath + ".tmp", dpi=300)
+        plt.savefig(imagepath + ".tmp.png", dpi=300)
     except Exception as e:
         print ("Cannot create image file... check perms {}".format(e))
     plt.close()
 
     # Do this so that we don't have to wait for the file to load while it's being updated.
     try:
-        shutil.copyfile(imagepath + ".tmp", imagepath)
-        os.remove(imagepath + ".tmp")
+        shutil.copyfile(imagepath + ".tmp.png", imagepath)
+        os.remove(imagepath + ".tmp.png")
     except Exception as e:
         print ("Error when copy/delete file: {}".format(e))
 
